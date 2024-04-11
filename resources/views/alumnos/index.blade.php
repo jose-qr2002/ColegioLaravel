33e6e001705366d2965e58c8fa79aa7d9c6a4b79
Jose  Quispe <jkqr2016@gmail.com>
2024-04-11 18:19:48 -0500

    Agregando estilos a la vista index de alumnos

--- resources/views/alumnos/index.blade.php
+++ resources/views/alumnos/index.blade.php
@@ -2,63 +2,47 @@
 
 @section('estilos')
     <style>
-        table {
-            width: 100%;
-            border: 1px solid #000;
-            border-spacing: 0;
-        }
-        th, td {
-            width: 20%;
-            text-align: left;
-            border: 1px solid #000;
-            padding: 0.3em;
-        }
-
-        th {
-            background: #afadad;
-            font-weight: 700;
-        }
-
-        caption {
-            padding: 0.3em;
-            color: #fff;
-            background: #000;
-        }
+        
     </style>
 @endsection
 
 @section('contenido')
-    <h1>Lista de Alumnos</h1>
-    <table>
-        <caption>
-            Tabla de Alumnos
-        </caption>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>DNI</th>
-                <th>Nombres</th>
-                <th>Apellidos</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($alumnos as $alumno)
-            <tr>
-                <td>{{ $alumno->id }}</td>
-                <td>{{ $alumno->dni }}</td>
-                <td>{{ $alumno->nombres }}</td>
-                <td>{{ $alumno->apellidos }}</td>
-                <td style="">
-                    <a href="{{ route('alumnos.edit', [$alumno->id]) }}" style="text-decoration: none; font-weight: 700; color: #807b4e;">Editar</a>
-                    <form action="{{ route('alumnos.destroy', [$alumno->id]) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" style="text-decoration: none; font-weight: 700; color: #f04242;padding: 0; background: none; border:none;cursor: pointer;">Eliminar</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+<div class="container">
+    <h1 class="text-uppercase fs-1 text-center mt-3">Lista de Alumnos</h1>
+    <a href="{{ route('alumnos.create') }}" class="btn btn-primary">Registrar Alumno</a>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered border-danger caption-top">
+            <caption>
+                Tabla de Alumnos
+            </caption>
+            <thead>
+                <tr class="table-dark">
+                    <th>ID</th>
+                    <th>DNI</th>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody class="table-warning">
+            @foreach($alumnos as $alumno)
+                <tr>
+                    <td>{{ $alumno->id }}</td>
+                    <td>{{ $alumno->dni }}</td>
+                    <td>{{ $alumno->nombres }}</td>
+                    <td>{{ $alumno->apellidos }}</td>
+                    <td style="">
+                        <a href="{{ route('alumnos.edit', [$alumno->id]) }}" style="text-decoration: none; font-weight: 700; color: #807b4e;">Editar</a>
+                        <form action="{{ route('alumnos.destroy', [$alumno->id]) }}" method="POST" style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" style="text-decoration: none; font-weight: 700; color: #f04242;padding: 0; background: none; border:none;cursor: pointer;">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
