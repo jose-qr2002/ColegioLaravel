e8ea1351393ac212d1fda1061435d8388a2e356a
Jose  Quispe <jkqr2016@gmail.com>
2024-04-11 19:13:11 -0500

    Mejorando estilos a la vista editar Alumno

--- resources/views/alumnos/edit.blade.php
+++ resources/views/alumnos/edit.blade.php
@@ -1,16 +1,47 @@
 @extends('cabecera')
+@section('estilos')
+    <style>
+        .container-form {
+            max-width: 430px;
+            margin: 0 auto;
+            padding: 10px;
+        }
+
+        input[type=number]::-webkit-inner-spin-button, 
+        input[type=number]::-webkit-outer-spin-button { 
+            -webkit-appearance: none; 
+            margin: 0;
+        }
+
+    </style>    
+@endsection
 
 @section('contenido')
-    <h2 style="text-align: center;">Editar Alumno</h2>
-    <div style="max-width: 400px; margin: 0 auto;">
-        <form method="POST" action="{{ route('alumnos.update', [$alumno->id]) }}" style="display: flex; flex-direction: column;">
-            @csrf
-            @method('PUT')
-            <input type="text" id="dni" name="dni" style="display: block; width: 100%; padding: 5px; margin-bottom: 10px;" placeholder="dni" value="{{ $alumno->dni }}">
-            <input type="text" id="nombres" name="nombres" style="display: block; width: 100%; padding: 5px; margin-bottom: 10px;" placeholder="nombres" value="{{ $alumno->nombres }}">
-            <input type="text" id="apellidos" name="apellidos" style="display: block; width: 100%; padding: 5px;" placeholder="apellidos" value="{{ $alumno->apellidos }}">
-
-            <button type="submit" style="padding: 10px; margin-top: 10px; font-size: 16px; background: #4770df; cursor: pointer; color: white; border: none;">Actualizar</button>
-        </form>
+<div class="container-form">
+    <h2 class="fs-1 mt-3 mb-3" style="text-align: center;">Editar Alumno</h2>
+    <div class="card">
+        <div class="card-body">
+            <a href="{{ route('alumnos.index') }}" class="btn btn-secondary btn-sm mb-2">Volver</a>
+            <h5 class="card-title">Llene los campos</h5>
+            <form method="POST" action="{{ route('alumnos.update', [$alumno->id]) }}">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <label for="dni" class="form-label">DNI</label>
+                    <input type="number" id="dni" name="dni" class="form-control" placeholder="Ingrese un N° de DNI" value="{{$alumno->dni}}">
+                </div>
+                <div class="mb-3">
+                    <label for="nombres" class="form-label">Nombres</label>
+                    <input type="text" id="nombres" name="nombres" class="form-control" placeholder="Ingrese su nombre" value="{{$alumno->nombres}}">
+                </div>
+                <div class="mb-3">
+                    <label for="apellidos" class="form-label">Apellidos</label>
+                    <input type="text" id="apellidos" name="apellidos" class="form-control" placeholder="Ingrese sus apellidos" value="{{$alumno->apellidos}}">
+                </div>
+        
+                <button type="submit" class="btn btn-dark">Guardar</button>
+            </form>
+        </div>
     </div>
+</div>
 @endsection
